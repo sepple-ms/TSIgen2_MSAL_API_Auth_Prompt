0ae9c14a11f0671f8ffdeb15691b3f4bc27e7490
Stefan Epple <stepple@hotmail.com>
2020-09-04 15:11:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿/*
+ The MIT License (MIT)
+
+Copyright (c) 2015 Microsoft Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+
+using System;
+using Microsoft.Identity.Client;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TSIgen2_MSAL_API_Auth_Prompt
+{
+    class Program
+    {
+        //Inspired by: https://github.com/Azure-Samples/active-directory-dotnet-native-aspnetcore-v2/blob/master/1.%20Desktop%20app%20calls%20Web%20API/TodoListClient/MainWindow.xaml.cs
+        //Reference: https://github.com/AzureAD/microsoft-authentication-library-for-dotnet/wiki/Acquiring-tokens-interactively
+
+        //1. Requires you to set "Treat application as a public client" in advanced settings of authentication in AD App Registration Blade
+        //2. You will have to add the individual user with corresponding rights to TSI "Data Access Policies"
+        //3. 
+        static void Main(string[] args)
+        {
+            try
+            {
+                RunAsync().GetAwaiter().GetResult();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        private static async Task RunAsync()
+        {
+            AuthenticationConfig config = AuthenticationConfig.ReadFromJsonFile("appsettings.json");
+
+            string[] scopes = new string[] { $"{config.ApiUrl}/.default" };
+            var app = PublicClientApplicationBuilder.Create(config.ClientId)
+                .WithRedirectUri("http://localhost")  //required if running locally: Browser opens, does the auth tango and returns token to app
+                .WithAuthority($"https://login.windows.net/{config.Tenant}")
+                .Build();
+            var accounts = await app.GetAccountsAsync();
+            AuthenticationResult result;
+            try
+            {
+                result = await app.AcquireTokenSilent(scopes, accounts.FirstOrDefault())
+                            .ExecuteAsync();
+            }
+            catch (MsalUiRequiredException)
+            {
+                result = await app.AcquireTokenInteractive(scopes)
+                            .ExecuteAsync();
+            }
+
+
+            if (result != null)
+            {
+                var httpClient = new HttpClient();
+                var apiCaller = new ProtectedApiCallHelper(httpClient);
+
+                //See for other APIs: https://docs.microsoft.com/en-us/rest/api/time-series-insights/reference-data-access-overview
+                await apiCaller.CallWebApiAndProcessResultASync($"{config.TsiEnvironmentBaseUrl}timeseries/modelSettings?api-version=2020-07-31", result.AccessToken, Display);
+            }
+        }
+
+        /// <summary>
+        /// Display the result of the Web API call
+        /// </summary>
+        /// <param name="result">Object to display</param>
+        private static void Display(JObject result)
+        {
+            foreach (JProperty child in result.Properties().Where(p => !p.Name.StartsWith("@")))
+            {
+                Console.WriteLine($"{child.Name} = {child.Value}");
+            }
+        }
+    }
+}
+
